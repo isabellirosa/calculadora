f16539f1d33c3b57334896d36d37438f4c06b03c
isabellirosa <isabelli.ifc@gmail.com>
2023-12-05 16:53:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let input = document.querySelector("#display");
+function limpar(){
+    input.value="";
+}
+function add_numero(numero) {
+    let operacao=input.value;
+    input.value=operacao+=numero;
+}
+function add_operador(opr){
+        let operacao=input.value;
+        input.value=operacao+=opr;
+}
+
+document.addEventListener('keydown', function (event){
+    if (!isNaN(event.key)|| event.key== "/"|| event.key=="*"||event.key =="+"|| event.key=="-"){
+        input.value = input.value + event.key;
+    }
+    else if(event.key=="%"){
+        input.value = input.value + "/100";
+    }
+    else if(event.key=="^"){
+        input.value = input.value + "**";
+    }
+    if ( event.key== "="|| event.key=="Enter"){
+        total();
+    }
+    if(event.key == "Backspace"|| event.key == "delete" || event.key == "Escape"){
+        limpar();
+    }
+});
+function total() {
+    document.querySelector("#history").innerHTML=input.value;
+    let resultado=input.value;
+    input.value=eval(resultado);
+    
+}
+function raiz_quadrada(){
+    document.querySelector("#history").innerHTML=input.value;
+    let resultado=input.value;
+    input.value=Math.sqrt(resultado);
+}
